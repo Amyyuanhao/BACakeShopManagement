ea32b76e1d1a974b21be69f545c1f487704b9fca
Amyyuanhao <haoyuanniuniu@gmail.com>
2024-05-23 11:33:38 +1200

    Write TODO list, Add classes, Welcome Message, Login

--- BACakeShopManagementApplication/BACakeShopManagementApplication/Program.cs
+++ BACakeShopManagementApplication/BACakeShopManagementApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,33 @@ namespace BACakeShopManagementApplication
     {
         static void Main(string[] args)
         {
+            //Initiate the store object
+            //TODO: Login 
+            //TODO:
+            //Menu skeleton
+            //- 1. AddManager
+            //- 2. RemoveManger
+            //- 3. AddCake
+            //- 4. RemoveCake
+            //- 5. AddWritingMessageOnCake
+            //- 6. AddDrink
+            // -7. RemoveDrink
+            // -8. SearchCakeByCustmerName
+            // -0. Exit
+
+            Console.WriteLine("-------WELCOME TO BA CAKE SHOP MANAGEMENT APPLICATION-------");
+            Console.WriteLine("------------------------------------------------------------");
+
+            Console.WriteLine("Please enter the use name: ");
+            string useName = Console.ReadLine();
+            Console.WriteLine("Please enter the password:");
+            string passWord = Console.ReadLine();
+            
+
+
+
+
+
         }
     }
 }

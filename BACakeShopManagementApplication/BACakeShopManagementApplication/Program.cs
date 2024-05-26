921dba2bf2b0c773821f71a3336a6db6a4b9a19d
XueshiBai <baixueshi215@gmail.com>
2024-05-26 21:33:47 +1200

    Bai creat login method

--- BACakeShopManagementApplication/BACakeShopManagementApplication/Program.cs
+++ BACakeShopManagementApplication/BACakeShopManagementApplication/Program.cs
@@ -26,116 +26,131 @@ namespace BACakeShopManagementApplication
             //TODO: -8. SearchCakeByCustmerName
             //TODO: -99. Exit
 
+            Store store = new Store("BA", "addr", "1121", "Baibai", "123");
 
-            var store = new Store("BA", "addr", "1121", "Baibai", "123");
+            //welcome message
+            Console.WriteLine("--------------------------------------------------------------");
+            Console.WriteLine("------- WELCOME TO BA CAKE SHOP MANAGEMENT APPLICATION -------");
+            Console.WriteLine("--------------------------------------------------------------");
 
-            Console.WriteLine("-------WELCOME TO BA CAKE SHOP MANAGEMENT APPLICATION-------");
-            Console.WriteLine("------------------------------------------------------------");
-            store.DisplayCakeStoreDitails("BA", "addr", "1121");
+            store.DisplayCakeStoreDitails("BA Cake Shop", "Sweeties street", "5201314");
 
-            var exitSystem = false;
-            
-            //writeline menu items
-
-            while (!exitSystem)
-            {
-                Console.WriteLine("Please select the operation: ");
-                Console.WriteLine("1. Add Manager");
-                Console.WriteLine("2. RemoveManager");
-                Console.WriteLine("3. Add cake");
-                Console.WriteLine("4. Remove cake");
-                Console.WriteLine("5. Add writing on cake");
-                Console.WriteLine("6. Add drink");
-                Console.WriteLine("7. Remove drink");
-                Console.WriteLine("8. Search Cake by custmer name");
-                Console.WriteLine("99. Exit");
-
-                string selectedAction = Console.ReadLine();
-
-                switch (selectedAction)
-                {
-                    case "1":
-                        Console.WriteLine("Adding new manager");
-                        //TODO: AddNewManager()
-                        Console.ReadKey();
-                        break;
-
-                    case "2":
-                       
-                        Console.WriteLine("Removing manager");
-                        //TODO: RemoveManager()
-                        break;
-
-                    case "3":
-                       
-                        Console.WriteLine("Adding new cake");
-                        Console.WriteLine("Please enter the cake name");
-                        var cakeName = Console.ReadLine();
-                        Console.WriteLine("Please enter the cake price");
-                        var cakePrice=Convert.ToDouble( Console.ReadLine());
-                        Console.WriteLine("Please enter the cake ID");
-                        var cakeId=Convert.ToInt32 (Console.ReadLine());
-                        store.AddCake(cakeName, cakePrice, cakeId);
-                        Console.WriteLine($"This {cakeName} has been added, the price is {cakePrice}, the ID is {cakeId}");
-                        //TODO: AddNewCake()
-                        break;
-
-                    case "4":
-                        
-                        Console.WriteLine("Removeing cake");
-                        //TODO: RemoveCake()
-                        break;
-
-                    case "5":
-                        
-                        Console.WriteLine("Adding writing on cake");
-                        //TODO: AddWritingOnCake()
-                        break;
-
-                    case "6":
-                        
-                        Console.WriteLine("Adding new drink");
-                        //TODO: AddNewDrink
-                        break;
-
-                    case "7":
-                        
-                        Console.WriteLine("Removing drink");
-                        //TODO: RemoveDrink()
-                        break;
-
-                    case "8":
-                        
-                        Console.WriteLine("Search cake (by custmer name)");
-                        //TODO: SearchCakeByUserName()
-                        break;
-
-                    case "99":
-                        
-                        Console.WriteLine("Exit");
-                        Environment.Exit(0);
-                        break;
-
-                    default:
-                        Console.WriteLine("Wrong choice.. Please try again");
-                        break;
-
-                }//end of switch
-                    
-
-            }//end of while
+            Console.WriteLine("------------------- Please Log In The App --------------------");
+            Console.WriteLine();
 
 
+            //Declare the Manager List
+            List<Manager> managers = new List<Manager>();
 
-            
-
-            
-
-
-
+            Manager Bai = new Manager("Bai", 001, "bai123");
+            Manager Amy = new Manager("Amy", 002, "amy123");
 
 
+            managers.Add(Bai);
+            managers.Add(Amy);
 
+            //Call loginmanager method
+            LoginPage.IdentityCheck(managers);
+            AppSelectionMenu(store);
         }
+
+      
+            public static void AppSelectionMenu( Store store)
+            {
+
+            bool exitSystem = false;
+
+                while (!exitSystem)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please select the operation: ");
+                    Console.WriteLine("1. Add Manager");
+                    Console.WriteLine("2. RemoveManager");
+                    Console.WriteLine("3. Add cake");
+                    Console.WriteLine("4. Remove cake");
+                    Console.WriteLine("5. Add writing on cake");
+                    Console.WriteLine("6. Add drink");
+                    Console.WriteLine("7. Remove drink");
+                    Console.WriteLine("8. Search Cake by custmer name");
+                    Console.WriteLine("99. Exit");
+
+                    string selectedAction = Console.ReadLine();
+
+                    switch (selectedAction)
+                    {
+                        case "1":
+                            Console.WriteLine("Adding new manager");
+                            //TODO: AddNewManager()
+                            Console.ReadKey();
+                            break;
+
+                        case "2":
+
+                            Console.WriteLine("Removing manager");
+                            //TODO: RemoveManager()
+                            break;
+
+                        case "3":
+
+                            Console.WriteLine("Adding new cake");
+                            Console.WriteLine("Please enter the cake name");
+                            var cakeName = Console.ReadLine();
+                            Console.WriteLine("Please enter the cake price");
+                            var cakePrice = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Please enter the cake ID");
+                            var cakeId = Convert.ToInt32(Console.ReadLine());
+                            store.AddCake(cakeName, cakePrice, cakeId);
+                            Console.WriteLine($"This {cakeName} has been added, the price is {cakePrice}, the ID is {cakeId}");
+
+                             //TODO: AddNewCake()
+                              break;
+
+                        case "4":
+
+                            Console.WriteLine("Removeing cake");
+                            //TODO: RemoveCake()
+                            break;
+
+                        case "5":
+
+                            Console.WriteLine("Adding writing on cake");
+                            //TODO: AddWritingOnCake()
+                            break;
+
+                        case "6":
+
+                            Console.WriteLine("Adding new drink");
+                            //TODO: AddNewDrink
+                            break;
+
+                        case "7":
+
+                            Console.WriteLine("Removing drink");
+                            //TODO: RemoveDrink()
+                            break;
+
+                        case "8":
+
+                            Console.WriteLine("Search cake (by custmer name)");
+                            //TODO: SearchCakeByUserName()
+                            break;
+
+                        case "99":
+
+                            Console.WriteLine("You are existing the app");
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("Wrong choice.. Please try again");
+                            break;
+
+                    }//end of switch
+
+
+                }//end of while
+
+                //writeline menu items
+            }
     }
 }
